cbd23c7b5ee791c01ba117dd00f9eb22bafd0516
Paul Bersch <paul.bersch@gsdesign.com>
2012-11-26 15:55:55 -0600

    continuing to set up a basic example project

--- example/example/wsgi.py
+++ example/example/wsgi.py
@@ -14,11 +14,12 @@ framework.
 
 """
 import os
+import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/apps/")
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../lib/python2.7/site-packages/")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../../virtualenv/lib/python2.7/site-packages/")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
 
